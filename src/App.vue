b34e7468e8d8f4d0b525d99a815ad9dc4c97c273
Yazin <github-7itbcg5@yaz.in>
2020-10-18 10:33:01 +0300

    Added modal

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,62 @@
 <template>
-  <div
-    id="app"
-    class="m-auto w-full h-full flex flex-col justify-between md:justify-center p-6 md:max-w-lg"
-  >
-    <form class="flex flex-col items-center">
-      <div>
-        <img alt="17.school logo" src="./assets/17.svg" class="w-16 m-10" />
+  <div id="app" class="h-full">
+    <div
+      v-if="modalVisible"
+      class="absolute w-full h-full left-0 top-0 z-10 flex justify-center items-center"
+      style="background: rgba(0, 0, 0, 0.8)"
+    >
+      <div class="clipped bg-gray-100 text-black font-light p-6 m-6 max-w-sm">
+        <p>
+          Thanks for signing up. We’ll reach out as soon as we start putting
+          together the next batch.
+        </p>
+        <p class="mt-4">Have you checked out our <a href="./blog" class="underline">./blog</a> yet?</p>
       </div>
+    </div>
+    <div
+      class="m-auto w-full h-full flex flex-col justify-between md:justify-center p-6 md:max-w-lg"
+    >
+      <form class="flex flex-col items-center">
+        <div>
+          <img alt="17.school logo" src="./assets/17.svg" class="w-16 m-10" />
+        </div>
 
-      <p>
-        We'll turn you into a developer in 6 months. Pay us nothing till you're
-        hired.
-      </p>
+        <p>
+          We'll turn you into a developer in 6 months. Pay us nothing till
+          you're hired.
+        </p>
 
-      <div class="w-full mt-12">
+        <div class="w-full mt-12">
+          <input
+            type="email"
+            placeholder="elon@spacex.com"
+            class="w-full placeholder-gray-100 gradient font-light pt-3 pb-3 pl-6 outline-none"
+          />
+        </div>
+
+        <div class="w-full mt-4">
+          <select
+            name="country"
+            class="w-full gradient font-light pt-3 pb-3 pl-6 outline-none"
+          >
+            <option value="bh">Bahrain</option>
+            <option value="sa">Saudi Arabia</option>
+            <option value="ae">UAE</option>
+          </select>
+        </div>
+
         <input
-          type="email"
-          placeholder="elon@spacex.com"
-          class="w-full placeholder-gray-100 gradient font-light pt-3 pb-3 pl-6 outline-none"
+          type="submit"
+          value="Count me in"
+          class="clipped mt-4 w-full bg-gray-100 rounded-none border-0 uppercase pt-3 pb-3 text-black cursor-pointer"
         />
+      </form>
+
+      <div class="md:mt-20 flex flex-col">
+        <a href="/faq">./faq</a>
+        <a href="/blog">./blog</a>
+        <a href="/contact-us">./contact-us</a>
       </div>
-
-      <div class="w-full mt-4">
-        <select
-          name="country"
-          class="w-full gradient font-light pt-3 pb-3 pl-6 outline-none"
-        >
-          <option value="bh">Bahrain</option>
-          <option value="sa">Saudi Arabia</option>
-          <option value="ae">UAE</option>
-        </select>
-      </div>
-
-      <input
-        type="submit"
-        value="Count me in"
-        class="mt-4 w-full bg-gray-100 rounded-none border-0 uppercase pt-3 pb-3 text-black cursor-pointer"
-      />
-    </form>
-
-    <div class="md:mt-20 flex flex-col">
-      <a href="/faq">./faq</a>
-      <a href="/blog">./blog</a>
-      <a href="/contact-us">./contact-us</a>
     </div>
   </div>
 </template>
@@ -50,6 +64,11 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      modalVisible: true,
+    };
+  },
   components: {},
   methods: {},
 };
@@ -59,7 +78,7 @@ export default {
 .gradient {
   background: linear-gradient(90.95deg, #333333 0%, #1a1a1a 99.48%);
 }
-input[type="submit"] {
+.clipped {
   clip-path: polygon(0 0, calc(100% - 14px) 0, 100% 14px, 100% 100%, 0 100%);
 }
 </style>
